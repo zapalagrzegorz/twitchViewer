3bf28cae957ee2dd8101c5e697935b6abad9183d
GrzegorzZapala <zapala.grzegorz@gmail.com>
2017-01-15 14:42:40 +0100

    Multiple Ajax developed scheme to make multiple API calls

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,181 +1,163 @@
 
 var engine = {
-	dataJSON: [],
-	// currentPage: 0,
-	self: this,
-	
-	users: ['freecodecamp', '1'],// array of users and their channels
+    dataJSON: [],
+    // currentPage: 0,
+    self: this,
 
-	// app makes request to Twitch API
-	getData: function (event) {
-		var self = this;
-		
-		// pick value from input form
-		var search = $("#twitch-search").val();
-		
-		// clear page data if no entry is given
-		var url = "esl_sc2";
-		
-		// jQuery call to Twitch API
-		var streams = {
-			type: "get",
-			url: "https://wind-bow.gomix.me/twitch-api/streams/esl_sc2",
-			headers: {
-				Accept: 'application/vnd.twitchtv.v3+json'
-			}
-		};
-		var streams2 = {
-			type: "get",
-			url: "https://wind-bow.gomix.me/twitch-api/streams/freecodecamp",
-			headers: {
-				Accept: 'application/vnd.twitchtv.v3+json'
-			}
-		};
-		var bool = 0;
-		
-		function setAjaxData(element) {
-			if(bool == 0){
-				bool = 1;
-				return $.ajax(streams);
-			}
-			else{
-				return $.ajax(streams2);
-			}
-		};
-		var arrayOfAjax = []
-		arrayOfAjax.push(streams, streams2)	;
+    users: ['freecodecamp', 'esl_sc2', '123'],// array of users and their channels
 
-		$.when.apply($, arrayOfAjax).done(
-			// self.dataJSON = [];
-			// var length = data.query.search.length;
-			// for (var i = 0; i < length; i++) {
-			// 	if (i % 10 === 0) {
-			// 		var arr = [];
-			// 	}
-			// 	// bez tego mamy tylko jeden obiekt
-			// 	var obj = {}; 
-			// 	obj.title = data.query.search[i].title;
-			// 	obj.snippet = data.query.search[i].snippet;
-			// 	arr.push(obj);
-			// 	if (i % 10 === 0) {
-			// 		self.dataJSON.push(arr);
-				// }
-			// }
-				fillCountryCapitals)
-				// function(data, data1) {
-    			// alert( "success" );
-				// console.log(data)
-				// console.log(data1)
-  			// },
-			//   	function(data, data1){
-			// 	console.log("failure");
-			//   }
-			//   );
-	function fillCountryCapitals (){
-		var countries = [];
-        var responses = arguments;
-        for(i in responses){
-            console.log(responses[i]);
-            // countries.push(responses[i][0][0].nativeName)
-        }  
-        // $('#countries').text(countries);
-	}
-	},
-			// 		$.get( "test.php" ).then(
-		//   function() {
-		//     alert( "$.get succeeded" );
-		//   }, function() {
-		//     alert( "$.get failed!" );
-		//   }
-		// );
+    // app makes request to Twitch API
+    getUsersData: function (event) {
+        var self = this;
 
-	// Random button
-	getRandomData: function () {
-		var self = this;
-		var url = "http://en.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0&origin=*";
-		$.getJSON(url, function (data) {
-			console.log(data);
-			console.log('random');
-			self.dataJSON = [];
-			var length = 1;
-			var arr = [];
-			var obj = {};
-			obj.title = data.query.random[0].title;
-			obj.snippet = "Random wiki title";
-			arr.push(obj);
-			self.dataJSON.push(arr);
-			self.showData();
-		})
-			.fail(function() {
-    			console.log( "error" );
-  			});
-	},
+        // pick value from input form
+        var search = $("#twitch-search").val();
 
-	// presents data on page
-	showData: function () {
-		var numPages = engine.dataJSON.length
-		setPagination(numPages);
-		setQueryResults(0);
+        getStreamsData();
 
-		// sets pagination, numPages: number
-		function setPagination(numPages) {
+        //do multiple parallel ajax requests
+        function getStreamsData() { 
+            var promises = [];
+            for (var i = 0, l = self.users.length; i < l; i++) {
+                var promise = ajaxRequestStream(self.users[i]);
+                promises.push(promise);
+            }
+            $.when.apply($, promises)
+                .done(handleSuccessStreams);
+        }
 
-			// setup HTML
-			var navigation = '<nav aria-label="Page navigation">' +
-				'<ul class="pagination">' +
-				'<li class="page-item">' +
-				'<a class="page-link previousPage" href="#" aria-label="Previous" data-pagenum="-1">' +
-				'<span aria-hidden="true">&laquo;</span>' +
-				'<span class="sr-only">Previous</span>' +
-				'</a>' +
-				'</li>';
-			for (var i = 0; i < numPages; i++) {
-				navigation += '<li class="page-item"><a class="page-link" data-pagenum="' + i + '" href="#">' + (i + 1) + '</a></li>';
-			}
-			navigation += '<li class="page-item">' +
-				'<a class="page-link" href="#" aria-label="Next" data-pagenum="++">' +
-				'<span aria-hidden="true">&raquo;</span>' +
-				'<span class="sr-only">Next</span>' +
-				'</a>'
-			'</li>'
-			'</ul>'
-			'</nav>';
+        // create single ajax requests for each user
+        function ajaxRequestStream(user) {
+            return $.ajax({
+                type: "get",
+                url: "https://wind-bow.gomix.me/twitch-api/streams/" + user,
+                headers: {
+                    Accept: 'application/vnd.twitchtv.v3+json'
+                },
+                success: function (response) {
+                    console.log(response);
+                },
+                error: function (response) {
+                    console.log("Error")
+                }
+            });
+        }
 
-			$('.nav').html(navigation);
+        // 
+        function handleSuccessStreams() {
+            var responses = arguments;
+            var promises = [];
+            for (i in responses) {
+                if (responses[i][0].stream === null) {
+					// collect all users which are not streaming
+                    promises.push(ajaxRequestUsers(self.users[i]));
+                }
+                else { }
+            }
+            if(promises.length !== 0){
+                getUsersData(promises);
+            }
+        }
 
-			// setup JS event handler
-			$('a.page-link').click(function (event) {
-				event.preventDefault();	
-				var pageNum = $(this).data('pagenum');
-				if (pageNum === -1 && (self.currentPage - 1) >= 0) {
-					setQueryResults(self.currentPage - 1);
-				}
-				else if (pageNum == "++" && (self.currentPage < numPages-1)) {
-					setQueryResults(self.currentPage+1);
-				}
-				else if (pageNum > 0) {
-					setQueryResults(pageNum);
-				}
-				else {
-					return false;
-				}
-			});
-		}
+        function handleError() {
+            console.log(error);
+        }
 
-		// sets results
-		function setQueryResults(pageNum) {
-			this.currentPage = pageNum;
-			$('#results').html('');
-			engine.dataJSON[pageNum].forEach(function (item) {
-				var result = $('<div class="wiki-results default-primary-color">').html('<a class="text-primary-color" href="https://en.wikipedia.org/wiki/' + item.title + '" target="_blank"><div class="wiki-header"><h2>' + item.title + '</h2></a></div><p class="wiki-extract">' + item.snippet + '...' + '</p>');
-				$('#results').append(result);
-			});
-		}
-	},
-	// clears page
-	clear: function(){
-		$('.nav').html('');
-		$('#results').html('');
-	}
+        // For users' not streaming do multiple parallel ajax requests
+        // @promises array of ajaxRequestUsers
+        function getUsersData(promises) { 
+            $.when.apply($, promises)
+                .done(handleSuccessUsers);
+        }
+        function ajaxRequestUsers(user) {
+            return $.ajax({
+                type: "get",
+                url: "https://wind-bow.gomix.me/twitch-api/users/" + user,
+                headers: {
+                    Accept: 'application/vnd.twitchtv.v3+json'
+                },
+                success: function (response) {
+                    console.log(response);
+                },
+                error: function (response) {
+                    console.log("Error")
+                }
+            });
+        }
+        function handleSuccessUsers() {
+            var responses = arguments;
+            for (i in responses) {
+                console.log(responses[i]);
+            }
+        }
+
+
+
+    // presents data on page
+    showData: function () {
+        var numPages = engine.dataJSON.length
+        setPagination(numPages);
+        setQueryResults(0);
+
+        // sets pagination, numPages: number
+        function setPagination(numPages) {
+
+            // setup HTML
+            var navigation = '<nav aria-label="Page navigation">' +
+                '<ul class="pagination">' +
+                '<li class="page-item">' +
+                '<a class="page-link previousPage" href="#" aria-label="Previous" data-pagenum="-1">' +
+                '<span aria-hidden="true">&laquo;</span>' +
+                '<span class="sr-only">Previous</span>' +
+                '</a>' +
+                '</li>';
+            for (var i = 0; i < numPages; i++) {
+                navigation += '<li class="page-item"><a class="page-link" data-pagenum="' + i + '" href="#">' + (i + 1) + '</a></li>';
+            }
+            navigation += '<li class="page-item">' +
+                '<a class="page-link" href="#" aria-label="Next" data-pagenum="++">' +
+                '<span aria-hidden="true">&raquo;</span>' +
+                '<span class="sr-only">Next</span>' +
+                '</a>'
+            '</li>'
+            '</ul>'
+            '</nav>';
+
+            $('.nav').html(navigation);
+
+            // setup JS event handler
+            $('a.page-link').click(function (event) {
+                event.preventDefault();
+                var pageNum = $(this).data('pagenum');
+                if (pageNum === -1 && (self.currentPage - 1) >= 0) {
+                    setQueryResults(self.currentPage - 1);
+                }
+                else if (pageNum == "++" && (self.currentPage < numPages - 1)) {
+                    setQueryResults(self.currentPage + 1);
+                }
+                else if (pageNum > 0) {
+                    setQueryResults(pageNum);
+                }
+                else {
+                    return false;
+                }
+            });
+        }
+
+        // sets results
+        function setQueryResults(pageNum) {
+            this.currentPage = pageNum;
+            $('#results').html('');
+            engine.dataJSON[pageNum].forEach(function (item) {
+                var result = $('<div class="wiki-results default-primary-color">').html('<a class="text-primary-color" href="https://en.wikipedia.org/wiki/' + item.title + '" target="_blank"><div class="wiki-header"><h2>' + item.title + '</h2></a></div><p class="wiki-extract">' + item.snippet + '...' + '</p>');
+                $('#results').append(result);
+            });
+        }
+    },
+    // clears page
+    clear: function () {
+        $('.nav').html('');
+        $('#results').html('');
+    }
 
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
 $().ready(function () {
-	$('#wiki-search-button').click($.proxy(engine.getData, engine));
-  $('#wiki-search-random').click($.proxy(engine.getRandomData, engine))
-  $(document).bind('keypress',pressed);
-  engine.getData();
+	// $('#wiki-search-button').click($.proxy(engine.getData, engine));
+  // $('#wiki-search-random').click($.proxy(engine.getRandomData, engine))
+  // $(document).bind('keypress',pressed);
+  engine.getUsersData();
 });
 
 function pressed(e)
 {
     if(e.keyCode === 13)
     {
-       engine.getData();
+       engine.getUsersData();
     }
 }
