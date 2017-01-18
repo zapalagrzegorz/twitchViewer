a785b2b7794f446020b26580bb273b3b303e3249
Grzegorz Zapała <GrzegorzZ@JETON.local>
2017-01-18 13:00:14 +0100

    handling user channel

--- scripts/engine.js
+++ scripts/engine.js
@@ -11,7 +11,7 @@ var engine = {
 		var self = this;
 
 		// pick value from input form
-		var search = $("#twitch-search").val();
+		// var search = $("#twitch-search").val();
 
 		getStreamsData();
 
@@ -32,8 +32,8 @@ var engine = {
 		// create single ajax requests for each user
 		function ajaxRequestStream(user) {
 			return $.ajax({
-				type: "get",
-				url: "https://wind-bow.gomix.me/twitch-api/streams/" + user,
+				type: 'get',
+				url: 'https://wind-bow.gomix.me/twitch-api/streams/' + user,
 				headers: {
 					Accept: 'application/vnd.twitchtv.v3+json'
 				}
@@ -44,15 +44,16 @@ var engine = {
 		function handleSuccessStreams() {
 			var responses = arguments;
 			var promises = [];
-			for (i in responses) {
+			for (var i in responses) {
 				if (responses[i][0].stream === null) {
-					// collect all users which are not streaming
+					// queue calls for all users which are not streaming
 					promises.push(ajaxRequestUsers(self.users[i]));
 				}
 				else {
-					showStreamData(responses[i][0].stream);
+					// showStreamData(responses[i][0].stream);
 				}
 			}
+			// make AJAX calls if there're any
 			if (promises.length !== 0) {
 				getUsersData(promises);
 			}
@@ -69,13 +70,13 @@ var engine = {
 			$.when.apply($, promises)
 				.done(handleSuccessUsers);
 
-				// gdy zwróci dane użytkowników, trzeba wywołać żadanie o chanell'ach  które są dostępne
-				// i odroczyć dane do czasu wykonania żadania
+			// gdy zwróci dane użytkowników, trzeba wywołać żadanie o chanell'ach  które są dostępne
+			// i odroczyć dane do czasu wykonania żadania
 		}
 		function ajaxRequestUsers(user) {
 			return $.ajax({
-				type: "get",
-				url: "https://wind-bow.gomix.me/twitch-api/users/" + user,
+				type: 'get',
+				url: 'https://wind-bow.gomix.me/twitch-api/users/' + user,
 				headers: {
 					Accept: 'application/vnd.twitchtv.v3+json'
 				}
@@ -83,97 +84,145 @@ var engine = {
 		}
 
 		function handleSuccessUsers() {
+			var usersData = [];
+			var promisesChannels = [];
 			var responses = arguments;
-			for (i in responses) {
-				if (!responses[i][0].hasOwnProperty('error')) {
-					showUserData(responses[i][0]);
+			for (var i in responses) {
+				if (responses[i][0].hasOwnProperty('error')) {
+					console.log(responses[i][0]);
+					// wyświetl, że użytkownika nie ma
+					// showUknownUserData()
 				}
 				else {
-					console.log(responses[i])
+					// user not streaming exists 
+					// collect all users to get their channel data, needs another AJAX call
+					console.log(responses[i][0]);
+					var user = {};
+					user.bio = responses[i][0].bio;
+					user.created_at = responses[i][0].created_at;
+					user.logo = responses[i][0].logo;
+					user.display_name = responses[i][0].display_name;
+					usersData.push(user);
+
+					promisesChannels.push(ajaxRequestChannels(responses[i][0].name));
+					// pobierz dla niego dane z channel
+					// promisesChannels.push(ajaxRequestChannel(responses[i][0].name));
 				}
 			}
+			if (promisesChannels.length !== 0) {
+				getChannelsData(promisesChannels, usersData);
+			}
+		}
+		function ajaxRequestChannels(user) {
+			return $.ajax({
+				type: 'get',
+				url: 'https://wind-bow.gomix.me/twitch-api/channels/' + user,
+				headers: {
+					Accept: 'application/vnd.twitchtv.v3+json'
+				}
+			});
+		}
+		function getChannelsData(promises, usersData) {
+			$.when.apply($, promises)
+				// jak przekazać tablice usersdata do funkcji pararametru .done !!!
+				.done(function () {
+					var responses = arguments;
+					for (var i in responses) {
+						console.log(responses[i][0]);
+						// var user = {};
+						// user.bio = responses[i][0].bio;
+						// users.created_at = responses[i][0].created_at;
+						// users.logo = responses[i][0].logo;
+						// users.dispaly_name = responses[i][0].dispaly_name;
+						// usersData.push(user);
+
+						// promisesChannels.push(ajaxRequestChannels(self.users[i]));
+						// pobierz dla niego dane z channel
+						// promisesChannels.push(ajaxRequestChannel(responses[i][0].name));
+					}
+				});
 		}
 
 		// receives object from Twitch API
 		// produces HTML output for stream object
-		function showStreamData(streamData){
+		// function showStreamData(streamData){
+		// 	console.log(streamData);
+		// 	userChannelData = {};
+		// 	var stream = '<article class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 channel">' +
+		// 					'<div class="channelPreview"><i class="fa fa-3x fa-play icon-play" aria-hidden="true"></i>' +
+		// 						'<div class="darken"><img class="img" src="'+ streamData.preview.large  +'"></div>' +
+		// 					'</div>' +
+		// 						'<div class="channelDescription">' +
+		// 							'<h3><a href="https://www.twitch.tv/game/' + streamData.channel.game + '">'+ streamData.channel.game +'</a></h3>' +
+		// 							'<div class="row">' +
+		// 								'<div class="col-10">' +
+		// 									'<h4><a href="' + streamData.channel.url + '">'+ streamData.channel.status +'</a></h4>' +
+		// 								'</div>' +
+		// 								'<div class="col-2" style="text-align: center">' +
+		// 									'<i class="fa fa-2x fa-user-circle" aria-hidden="true"></i>' +
+		// 									'<p>' + streamData.viewers +' online</p>' +
+		// 								'</div>' +
+		// 							'</div>' +
+		// 							'<div class="row">' +
+		// 								'<div class="col-2" style="padding-right:0;">' +
+		// 									'<a href="' + streamData.channel.url + '">' +
+		// 										'<img class="img" src="'+ streamData.channel.logo + '">' +
+		// 									'</a>' +
+		// 								'</div>' +
+		// 							'<div class="col-10">' +
+		// 								'<p><a href="' + streamData.channel.url + '">' + streamData.channel.display_name + '</a></p>' +
+		// 								'<p>followers: ' + streamData.channel.followers +'</p>' +
+		// 								'<p>Launched: ' + (streamData.channel.created_at).substring(0,10) + '</p>' +
+		// 							'</div>' +
+		// 						'</div>' +
+		// 					'</div>' +
+		// 				'</article>';
+		// 	$('#streamContent').append(stream);
+		// }
+		function showUserData(streamData) {
 			console.log(streamData);
-			userChannelData = {};
-			var stream = '<article class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 channel">' +
-							'<div class="channelPreview"><i class="fa fa-3x fa-play icon-play" aria-hidden="true"></i>' +
-								'<div class="darken"><img class="img" src="'+ streamData.preview.large  +'"></div>' +
-							'</div>' +
-								'<div class="channelDescription">' +
-									'<h3><a href="https://www.twitch.tv/game/' + streamData.channel.game + '">'+ streamData.channel.game +'</a></h3>' +
-									'<div class="row">' +
-										'<div class="col-10">' +
-											'<h4><a href="' + streamData.channel.url + '">'+ streamData.channel.status +'</a></h4>' +
-										'</div>' +
-										'<div class="col-2" style="text-align: center">' +
-											'<i class="fa fa-2x fa-user-circle" aria-hidden="true"></i>' +
-											'<p>' + streamData.viewers +' online</p>' +
-										'</div>' +
-									'</div>' +
-									'<div class="row">' +
-										'<div class="col-2" style="padding-right:0;">' +
-											'<a href="' + streamData.channel.url + '">' +
-												'<img class="img" src="'+ streamData.channel.logo + '">' +
-											'</a>' +
-										'</div>' +
-									'<div class="col-10">' +
-										'<p><a href="' + streamData.channel.url + '">' + streamData.channel.display_name + '</a></p>' +
-										'<p>followers: ' + streamData.channel.followers +'</p>' +
-										'<p>Launched: ' + (streamData.channel.created_at).substring(0,10) + '</p>' +
-									'</div>' +
-								'</div>' +
-							'</div>' +
-						'</article>';
-			$('#streamContent').append(stream);
-		}
-		function showUserData(streamData){
-			console.log(streamData);
-			var userChannelData = {};
-			// to wywołanie dopiero jest zakolejkowane - musi być przeniesione do 
-			$.ajax({
-				type: "get",
-				url: "https://wind-bow.gomix.me/twitch-api/channels/" + streamData.name,
-				headers: {
-					Accept: 'application/vnd.twitchtv.v3+json'
-				},
-				success: function (data) {
-					console.log(data);
-					userChannelData.profile_banner = data.profile_banner;
-					userChannelData.followers = data.followers;
-					// userChannelData.push(data) 
-				}
-			});	
-			console.log(userChannelData);
-			var stream = '<article class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 channel">' +
-							'<div class="channelPreview">' +
-								'<img class="img" src="'+userChannelData.profile_banner+'">' +
-							'</div>' +
-								'<div class="channelDescription">' +
-								// tu by można wrzucić jeszcze jedno zawołanie dla celów zdjecia offline
-									'<h3><a href="https://www.twitch.tv/' + streamData.name + '">'+ streamData.name +'</a></h3>' +
-									'<div class="row">' +
-										'<div class="col-12">' +
-											'<p class="user-bio">' + streamData.bio.substring(0,146) +'...</p>' +
-										'</div>' +
-									'</div>' +
-									'<div class="row">' +
-										'<div class="col-2" style="padding-right:0;">' +
-											'<a href="https://www.twitch.tv/' + streamData.name + '">' +
-												'<img class="img" src="'+ streamData.logo + '">' +
-											'</a>' +
-										'</div>' +
-									'<div class="col-10">' + +
-										'<p>followers: ' + userChannelData.profile_banner + '</p>' +
-										'<p>Launched: ' + (streamData.created_at).substring(0,10) + '</p>' +
-									'</div>' +
-								'</div>' +
-							'</div>' +
-						'</article>';
-			$('#streamContent').append(stream);
+			// var userChannelData = {};
+			// // to wywołanie dopiero jest zakolejkowane - musi być przeniesione do 
+			// $.ajax({
+			// 	type: "get",
+			// 	url: "https://wind-bow.gomix.me/twitch-api/users/" + streamData.name,
+			// 	headers: {
+			// 		Accept: 'application/vnd.twitchtv.v3+json'
+			// 	},
+			// 	success: function (data) {
+			// 		console.log(data);
+			// 		userChannelData.profile_banner = data.profile_banner;
+			// 		userChannelData.followers = data.followers;
+			// 		// userChannelData.push(data) 
+			// 	}
+			// });	
+			// console.log(userChannelData);
+			// var stream = '<article class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 channel">' +
+			// 				'<div class="channelPreview">' +
+			// 					'<img class="img" src="'+userChannelData.profile_banner+'">' +
+			// 				'</div>' +
+			// 					'<div class="channelDescription">' +
+			// 					// tu by można wrzucić jeszcze jedno zawołanie dla celów zdjecia offline
+			// 						'<h3><a href="https://www.twitch.tv/' + streamData.name + '">'+ streamData.name +'</a></h3>' +
+			// 						'<div class="row">' +
+			// 							'<div class="col-12">' +
+			// 								'<p class="user-bio">' + streamData.bio.substring(0,146) +'...</p>' +
+			// 							'</div>' +
+			// 						'</div>' +
+			// 						'<div class="row">' +
+			// 							'<div class="col-2" style="padding-right:0;">' +
+			// 								'<a href="https://www.twitch.tv/' + streamData.name + '">' +
+			// 									'<img class="img" src="'+ streamData.logo + '">' +
+			// 								'</a>' +
+			// 							'</div>' +
+			// 						'<div class="col-10">' + +
+			// 							'<p>followers: ' + userChannelData.profile_banner + '</p>' +
+			// 							'<p>Launched: ' + (streamData.created_at).substring(0,10) + '</p>' +
+			// 						'</div>' +
+			// 					'</div>' +
+			// 				'</div>' +
+			// 			'</article>';
+			// $('#streamContent').append(stream);
 		}
 	}
-}
+};
--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,13 @@ $().ready(function () {
 	// $('#wiki-search-button').click($.proxy(engine.getData, engine));
   // $('#wiki-search-random').click($.proxy(engine.getRandomData, engine))
   // $(document).bind('keypress',pressed);
-  engine.getUsersData();
+	engine.getUsersData();
 });
 
-function pressed(e)
-{
-    if(e.keyCode === 13)
-    {
-       engine.getUsersData();
-    }
-}
+// function pressed(e)
+// {
+// 	if(e.keyCode === 13)
+//   {
+// 		engine.getUsersData();
+// 	}
+// }
